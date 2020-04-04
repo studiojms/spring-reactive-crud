b9642a66d29bd7ba0f822179b1ab7d0c06ad0415
Jefferson M Souza <jefferson.msouza86@gmail.com>
2020-04-03 23:10:47 -0300

    Project setup

---
+++ src/test/java/com/studiojms/springreactivecrud/SpringReactiveCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studiojms.springreactivecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
